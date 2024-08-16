e06ea7b783a67471d141987acc2427cd22bcb837
Murat  99 <privatenmejl8@gmail.com>
2024-08-16 19:35:23 +0200

    Class03 Homework added

---
+++ Class03Homework/BookApp/BookApp.WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,129 @@
+﻿using BookApp.WebAPI;
+using BookApp.WebAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+//5. Add GET method that returns all books
+
+//6.Add GET method that returns one book by sending index in the query string
+
+//7. Add GET method that returns one book by filtering by author and title (use query string parameters)
+
+//8. Add POST method that adds new book to the list of books (use the FromBody attribute) 
+
+
+namespace BookApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Book>> GetAll()
+        {
+            try
+            {
+                return StatusCode(StatusCodes.Status200OK, StaticDb.Books);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        [HttpGet("queryString")]
+        public ActionResult<Book> GetByQueryString([FromQuery] int? index)
+        {
+            try
+            {
+                if (index == null)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "Index is a requred parameter");
+                }
+                if (index < 0)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "The index can not be negative!");
+
+                }
+                if (index >= StaticDb.Books.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, $"There is no resource on index {index}");
+                }
+                return StatusCode(StatusCodes.Status200OK, StaticDb.Books[(index-1).Value]);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+        [HttpGet("filter")]
+        public ActionResult<List<Book>> FilterByAuthorAndTitle([FromQuery] string? author, [FromQuery] string? title)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(author) && string.IsNullOrEmpty(title))
+                {
+                    return Ok(StaticDb.Books);
+                }
+                if (string.IsNullOrEmpty(author))
+                {
+                    List<Book> filteredByTitle = StaticDb.Books.Where(x => x.Title.ToLower().Contains(title.ToLower())).ToList();
+                    return Ok(filteredByTitle);
+                }
+                if (string.IsNullOrEmpty(title))
+                {
+                    List<Book> filteredByAuthor = StaticDb.Books.Where(x => x.Author.ToLower().Contains(author.ToLower())).ToList();
+                    return Ok(filteredByAuthor);
+                }
+                List<Book> booksDb = StaticDb.Books
+                .Where(x => x.Title.ToLower().Contains(title.ToLower())
+                 && x.Author.ToLower().Contains(author.ToLower()))
+                .ToList();
+                return Ok(booksDb);
+
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+        [HttpPost]
+        public IActionResult CreateBook([FromBody] Book book)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(book.Author) || string.IsNullOrEmpty(book.Title))
+                {
+                    return BadRequest("The book must have an author and a title");
+                }
+                StaticDb.Books.Add(book);
+                return StatusCode(StatusCodes.Status201Created, "Book was created.");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+        //## Bonus
+
+        //Add POST method that accepts list of  books from the body of the request and returns their titles as a list of strings.
+        [HttpPost("bookTitle")]
+        public IActionResult GetBookTitles([FromBody] List<Book> books)
+        {
+            try
+            {
+                if (books is not null && books.Count > 0)
+                {
+                    var titles = books.Select(b => b.Title).ToList();
+                    return Ok(titles);
+                }
+                return BadRequest("The list of books is empty!");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+
+
+    }
+}
---
+++ Class03Homework/BookApp/BookApp.WebAPI/StaticDb.cs
@@ -0,0 +1,26 @@
+﻿using BookApp.WebAPI.Model;
+
+namespace BookApp.WebAPI
+{
+
+    public static class StaticDb
+    {
+        public static List<Book> Books { get; } = new List<Book>
+        {
+            new Book { Id = 1, Author = "George Orwell", Title = "1984" },
+            new Book { Id = 2, Author = "Harper Lee", Title = "To Kill a Mockingbird" },
+            new Book { Id = 3, Author = "F. Scott Fitzgerald", Title = "The Great Gatsby" },
+            new Book { Id = 4, Author = "J.R.R. Tolkien", Title = "The Lord of the Rings" },
+            new Book { Id = 5, Author = "Jane Austen", Title = "Pride and Prejudice" },
+            new Book { Id = 6, Author = "Mark Twain", Title = "The Adventures of Huckleberry Finn" },
+            new Book { Id = 7, Author = "Herman Melville", Title = "Moby Dick" },
+            new Book { Id = 8, Author = "Leo Tolstoy", Title = "War and Peace" },
+            new Book { Id = 9, Author = "Charles Dickens", Title = "A Tale of Two Cities" },
+            new Book { Id = 10, Author = "Emily Brontë", Title = "Wuthering Heights" },
+            new Book { Id = 11, Author = "Gabriel García Márquez", Title = "One Hundred Years of Solitude" },
+            new Book { Id = 12, Author = "Aldous Huxley", Title = "Brave New World" },
+            new Book { Id = 13, Author = "Fyodor Dostoevsky", Title = "Crime and Punishment" },
+            new Book { Id = 14, Author = "Ernest Hemingway", Title = "The Old Man and the Sea" }
+        };
+    }
+}
